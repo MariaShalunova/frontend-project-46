cf074e6910c7d5314592dea155ccd017579bf432
Maria Shalunova <shalunova00@gmail.com>
2024-06-11 12:14:36 +0300

    add gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+
+program
+    .name('gendiff.js')
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0');
+
+program.parse();
