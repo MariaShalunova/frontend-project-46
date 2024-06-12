45a226f7cb4e11680727d1752b52709a5dbc1789
Maria Shalunova <shalunova00@gmail.com>
2024-06-12 18:28:54 +0300

    step 1

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
+const { program } = require('commander')
 
 program
-    .name('gendiff.js')
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
+  .name('gendiff.js')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
 
-program.parse();
+program.parse()
