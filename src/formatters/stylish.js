ea2902fd78817978f673a52ec5de4f0e3cfc5e7d
Maria Shalunova <shalunova00@gmail.com>
2024-10-17 04:15:29 +0300

    fix step 7

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -7,41 +7,52 @@ const status = {
   nested: ' ',
 };
 
-const getIndent = (depth) => '  '.repeat(depth);
+const getIndent = (depth, correctSize = 0) => {
+  const replacer = '  ';
+  const spacesCount = 2;
+  const indentSize = depth * spacesCount;
+  const currentIndent = replacer.repeat(indentSize - correctSize);
+  const bracketIndent = replacer.repeat(indentSize - spacesCount);
+  return { currentIndent, bracketIndent };
+};
 
-const formatBraces = (lines, depth) => [
+const formatBraces = (lines, depth) => {
+  const { currentIndent, bracketIndent } = getIndent(depth);
+  return [
   '{',
   ...lines,
-  `${getIndent(depth)}}`,
+  `${bracketIndent}}`,
 ].join('\n');
+};
 
 const stringify = (value, depth) => {
+  const { currentIndent, bracketIndent } = getIndent(depth);
   if (!_.isObject(value)) {
     return `${value}`;
   }
   const lines = Object
     .entries(value)
-    .map(([key, val]) => `${getIndent(depth + 1)}${key}: ${stringify(val, depth + 1)}`);
+    .map(([key, val]) => `${currentIndent}${key}: ${stringify(val, depth + 1)}`);
   
   return formatBraces(lines, depth);
 };
 
 const formatNode  = (node, depth = 1) => {
   const { key, value, type, value1, value2, children } = node;
-  const indent = getIndent(depth);
+  const { currentIndent, bracketIndent } = getIndent(depth, 1);
 
     switch(type) {
       case 'added':
-        return `${indent}${status.added} ${key}: ${stringify(value, depth + 1)}`;
+        return `${currentIndent}${status.added} ${key}: ${stringify(value, depth + 1)}`;
       case 'deleted':
-        return `${indent}${status.deleted} ${key}: ${stringify(value, depth + 1)}`;
+        return `${currentIndent}${status.deleted} ${key}: ${stringify(value, depth + 1)}`;
       case 'changed':
-        return [`${indent}${status.deleted} ${key}: ${stringify(value1, depth + 1)}`, 
-                `${indent}${status.added} ${key}: ${stringify(value2, depth + 1)}`].join('\n');
+        return [`${currentIndent}${status.deleted} ${key}: ${stringify(value1, depth + 1)}`, 
+                `${currentIndent}${status.added} ${key}: ${stringify(value2, depth + 1)}`].join('\n');
       case 'nested':
-        return `${indent}${status.nested} ${key}: ${stylish (children, depth + 1)}`;
+        return `${currentIndent}${status.nested} ${key}: ${stylish (children, depth + 1)}`;
       case 'unchanged':
-        return `${indent}${status.unchanged} ${key}: ${stringify(value, depth + 1)}`;
+        return `${currentIndent}${status.unchanged} ${key}: ${stringify(value, depth + 1)}`;
       default:
         throw new Error(`Unknown type: ${type}`);
     }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-import { readFileSync, writeFileSync } from 'fs';
+import { readFileSync} from 'fs';
 import path from 'path';
 import { cwd } from 'process';
 import parse from './parsers.js';
@@ -8,7 +8,6 @@ import formatter from './formatters/index.js';
 
 const getPath = (filepath) => path.resolve(cwd(), filepath);
 const readFile = (filepath) => readFileSync(getPath(filepath), 'utf-8');
-const saveFile = (filepath, data) => writeFileSync(filepath, data, 'utf-8');
 const getExtension = (filepath) => path.extname(filepath);
 
 const gendiff = (filepath1, filepath2, format = 'stylish') => {
@@ -17,11 +16,7 @@ const gendiff = (filepath1, filepath2, format = 'stylish') => {
 
   const result = compareDeep(obj1, obj2);
 
-  const data = formatter(result, format);
-
-  const filename = '__fixtures__/expectedResultJson.txt';
-  saveFile(filename, data);
-  return data;
+  return formatter(result, format);
 };
 
 export default gendiff;
