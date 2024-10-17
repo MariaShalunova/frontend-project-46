be615f207d8002ef1315c0fb6cddbd31b520bbbc
Maria Shalunova <shalunova00@gmail.com>
2024-10-17 04:39:41 +0300

    fix eslint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,10 +5,10 @@ import gendiff from '../index.js';
 const program = new Command();
 
 program
-  .name('gendiff.js')
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     const diff = gendiff(filepath1, filepath2, program.opts().format);
