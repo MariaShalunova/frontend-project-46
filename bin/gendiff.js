8a79b7fe245b07fbcaa18e5737de31b949e77494
Maria Shalunova <shalunova00@gmail.com>
2024-10-17 04:59:43 +0300

    update gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,16 +5,16 @@ import gendiff from '../index.js';
 const program = new Command();
 
 program
+  .name('gendiff')
   .version('1.0.0')
-  .usage('[options] <filepath1> <filepath2>')
+  .usage('[options] <first_config> <second_config>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format', 'stylish')
+  .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     const diff = gendiff(filepath1, filepath2, program.opts().format);
     console.log(diff);
   });
-
 
 program.parse(process.argv);
