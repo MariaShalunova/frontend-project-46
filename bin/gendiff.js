d6de58049bf32343a5519491676aa72137e10644
Maria Shalunova <shalunova00@gmail.com>
2024-10-17 04:56:26 +0300

    add .usage in gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,9 +5,9 @@ import gendiff from '../index.js';
 const program = new Command();
 
 program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .usage('[options] <filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format', 'stylish')
   .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
@@ -15,5 +15,6 @@ program
     const diff = gendiff(filepath1, filepath2, program.opts().format);
     console.log(diff);
   });
+
 
 program.parse(process.argv);
