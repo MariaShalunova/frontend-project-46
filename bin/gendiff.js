85cbf8ea8db94a45fca78df201d8ab683be6fa2c
Maria Shalunova <shalunova00@gmail.com>
2024-10-17 04:42:42 +0300

    fix gendiff param

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
+  .version('1.0.0', '-V, --version', 'output version information')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
