8f15d98d8b69e6f5a9df618f3a80ad2276ceb0d0
Maria Shalunova <shalunova00@gmail.com>
2024-10-17 04:51:31 +0300

    fix gendiff param

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,12 +7,12 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .option('-v, --version', 'output the version number')
+  .version('1.0.0')
   .option('-f, --format [type]', 'output format', 'stylish')
   .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2, option) => {
-    const diff = gendiff(filepath1, filepath2, option.format);
+  .action((filepath1, filepath2) => {
+    const diff = gendiff(filepath1, filepath2, program.opts().format);
     console.log(diff);
   });
 
