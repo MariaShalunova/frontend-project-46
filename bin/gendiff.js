b331c117329c252e9dd1eb8f23192eae3d1b685b
Maria Shalunova <shalunova00@gmail.com>
2024-10-17 04:46:10 +0300

    fix gendiff param

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,8 +10,8 @@ program
   .version('1.0.0', '-V, --version', 'output version information')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    const diff = gendiff(filepath1, filepath2, program.opts().format);
+  .action((filepath1, filepath2, option) => {
+    const diff = gendiff(filepath1, filepath2, option.format);
     console.log(diff);
   });
 
