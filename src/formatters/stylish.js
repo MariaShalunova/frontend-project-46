22f53a946215585fd10983e82a41a2dec00b37a3
Maria Shalunova <shalunova00@gmail.com>
2024-10-21 16:36:17 +0300

    refactor stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -37,33 +37,29 @@ const stringify = (value, depth) => {
 };
 
 const stylish = (node, depth = 1) => {
-  const formatNode = (currentNode) => {
-    const {
-      key, value, type, value1, value2, children,
-    } = currentNode;
-    const { currentIndent } = getIndent(depth, 1);
+    const result = node.flatMap((currentNode) => {
+      const {key, value, type, value1, value2, children, } = currentNode;
+      const { currentIndent } = getIndent(depth, 1);
 
-    switch (type) {
-      case 'added':
-        return `${currentIndent}${status.added} ${key}: ${stringify(value, depth + 1)}`;
-      case 'deleted':
-        return `${currentIndent}${status.deleted} ${key}: ${stringify(value, depth + 1)}`;
-      case 'changed':
-        return [
-          `${currentIndent}${status.deleted} ${key}: ${stringify(value1, depth + 1)}`,
-          `${currentIndent}${status.added} ${key}: ${stringify(value2, depth + 1)}`,
-        ].join('\n');
-      case 'nested':
-        return `${currentIndent}${status.nested} ${key}: ${stylish(children, depth + 1)}`;
-      case 'unchanged':
-        return `${currentIndent}${status.unchanged} ${key}: ${stringify(value, depth + 1)}`;
-      default:
-        throw new Error(`Unknown type: ${type}`);
-    }
-  };
-
-  const result = node.map((item) => formatNode(item));
-  return formatBraces(result, depth);
+      switch (type) {
+        case 'added':
+          return `${currentIndent}${status.added} ${key}: ${stringify(value, depth + 1)}`;
+        case 'deleted':
+          return `${currentIndent}${status.deleted} ${key}: ${stringify(value, depth + 1)}`;
+        case 'changed':
+          return [
+            `${currentIndent}${status.deleted} ${key}: ${stringify(value1, depth + 1)}`,
+            `${currentIndent}${status.added} ${key}: ${stringify(value2, depth + 1)}`,
+          ].join('\n');
+        case 'nested':
+          return `${currentIndent}${status.nested} ${key}: ${stylish(children, depth + 1)}`;
+        case 'unchanged':
+          return `${currentIndent}${status.unchanged} ${key}: ${stringify(value, depth + 1)}`;
+        default:
+          throw new Error(`Unknown type: ${type}`);
+      }
+    });
+  return formatBraces(result, depth);  
 };
 
 export default stylish;
